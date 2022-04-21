236ce91a5b0b5fc4e94239516e59cb3fdbdf36f9
HGFdal <75576995+HGFdal@users.noreply.github.com>
2022-04-21 20:34:14 +0800

    Create boom.js

---
+++ crack/boom.js
@@ -0,0 +1,7 @@
+
+hostname = apimboom2.globaldelight.net,
+#Boom
+https?://apimboom2\.globaldelight\.net/itunesreceipt_v2\.php url script-response-body https://raw.githubusercontent.com/TienPD3/iAPFake/main/Scripts/iAPVip/BoomLifetime.js
+
+
+
